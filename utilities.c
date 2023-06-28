a2a5117b0b1591debef667a17beb65992fa82e1a
Johannes letsoalo <letsoalojohannes6@gmail.com>
2023-06-27 19:55:20 -0700

    betty fix

--- utilities.c
+++ utilities.c
@@ -27,7 +27,8 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 
 	if (ascii_code < 0)
 		ascii_code *= -1;
-	buffer[i++] = '\\';buffer[i++] = 'x';
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
 	buffer[i++] = map_to[ascii_code / 16];
 	buffer[i] = map_to[ascii_code % 16];
 	return (3);
